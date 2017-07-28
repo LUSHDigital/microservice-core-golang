fac4cb57cfbe4fda416e3e27feed29d321186f28
Benjamin Cable <ben.cable@lush.co.uk>
2017-07-28 15:05:54 +0100

    Adds UnmarshalJSON to Data struct
    Also adds tests around it and supresses log output during benchmarks

--- response/response.go
+++ response/response.go
@@ -25,7 +25,44 @@ type Response struct {
 // Type ends up being the name of the key containing the collection of Content
 type Data struct {
 	Type    string
-	Content map[string]interface{}
+	Content interface{}
+}
+
+// UnmarshalJSON implements the Unmarshaler interface
+// this implementation will fill the type in the case we're been provided a valid single collection
+// and set the content to the contents of said collection.
+// for every other options, it behaves like normal.
+// Despite the fact that we are not suposed to marshal without a type set
+// This is purposefuly left open to unmarshal without a collection name set, in case you may want to set it later,
+// and for interop with other systems which may not send the collection properly.
+func (d *Data) UnmarshalJSON(b []byte) error {
+	if err := json.Unmarshal(b, &d.Content); err != nil {
+		log.Printf("cannot unmarshal data: %v", err)
+	}
+
+	data, ok := d.Content.(map[string]interface{})
+	if ok {
+		// count how many collections were provided
+		var count int
+		for _, value := range data {
+			if _, ok := value.(map[string]interface{}); ok {
+				count++
+			}
+		}
+		if count > 1 {
+			// we can stop there since this is not a single collection
+			return nil
+		}
+
+		for key, value := range data {
+			if _, ok := value.(map[string]interface{}); ok {
+				d.Type = key
+				d.Content = data[key]
+			}
+		}
+	}
+
+	return nil
 }
 
 // Valid ensures the Data passed to the response is correct
